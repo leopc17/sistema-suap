76ef3da366420dc3d22598c2de7cc87c074c5d1d
leopc17 <LeonardoEAF45@gmail.com>
2024-04-17 10:30:19 -0300

    criação do método removerAcentos

--- src/utils/GeradorEmail.java
+++ src/utils/GeradorEmail.java
@@ -14,4 +14,24 @@ public class GeradorEmail {
     public static String gerarEmailInstitucional(Professor p) {
         return p.getNome().toLowerCase().replace(" ", ".") + dominioServidor;
     }
+
+    private String removerAcentos (String texto){
+        texto = texto.replaceAll("[ÂÀÁÄÃ]","A");
+        texto = texto.replaceAll("[âãàáä]","a");
+        texto = texto.replaceAll("[ÊÈÉË]","E");
+        texto = texto.replaceAll("[êèéë]","e");
+        texto = texto.replaceAll("ÎÍÌÏ","I");
+        texto = texto.replaceAll("îíìï","i");
+        texto = texto.replaceAll("[ÔÕÒÓÖ]","O");
+        texto = texto.replaceAll("[ôõòóö]","o");
+        texto = texto.replaceAll("[ÛÙÚÜ]","U");
+        texto = texto.replaceAll("[ûúùü]","u");
+        texto = texto.replaceAll("Ç","C");
+        texto = texto.replaceAll("ç","c");
+        texto = texto.replaceAll("[ýÿ]","y");
+        texto = texto.replaceAll("Ý","Y");
+        texto = texto.replaceAll("ñ","n");
+        texto = texto.replaceAll("Ñ","N");
+        return texto;
+    }
 }
